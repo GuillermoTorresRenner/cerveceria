bc4fc311e0a603de103e14fb18d6d0e898fc4454
Guille <torresrennerguillermo@gmail.com>
2021-01-01 21:01:53 -0300

    Se definen las funciones de operación de relays asociadas a la recepción de caracteres por medio de comunicación I2C

--- desarollo/receptor/receptor.ino
+++ desarollo/receptor/receptor.ino
@@ -14,9 +14,9 @@
 #define salidaIntercambiador 8 //salida 8 relay 8 canales
 
 /*Las siguientes variables refieren a los pines que manejan los canales del relay 4 los cuales funcionarán a 220V manejando la bomba, la resistencia del macerador y el motor  de la moledora */
-#define bomba 9
-#define molino 10
-#define macerador 11
+#define bomba 9 //salida 1 relay 4 canales
+#define bombaLavado 10 //salida 2 relay 4 canales
+#define macerador 11 //salida 3 relay 4 canales
 
 /*Definición de variables auxiliares*/
 char recepcion;
@@ -48,7 +48,7 @@ void setup() {
  digitalWrite(9,HIGH);
  digitalWrite(10,HIGH);
  digitalWrite(11,HIGH);
- 
+
 
 
   Wire.begin(1);
@@ -72,36 +72,42 @@ void receiveEvent(int howMany){
   if(Wire.available()>0){
      recepcion=Wire.read();
   switch(recepcion){
-    case 'r':
-      digitalWrite(2,HIGH);
-      digitalWrite(3,LOW);
-      digitalWrite(4,LOW);
+   
+      case 'a':entradaAguaSinFiltrar();
       break;
 
-      case 'a':
-      digitalWrite(2,LOW);
-      digitalWrite(3,HIGH);
-      digitalWrite(4,LOW);
+      case 'b':entradaAguaFiltrada();
       break;
 
-      case 'v':
-      digitalWrite(2,LOW);
-      digitalWrite(3,LOW);
-      digitalWrite(4,HIGH);
+      case 'c':entradaExterior();
       break;
 
-       case 'n':
-      digitalWrite(2,LOW);
-      digitalWrite(3,LOW);
-      digitalWrite(4,LOW);
+      case 'd':trasiegoOlla();
       break;
 
-       case 't':
-      digitalWrite(2,HIGH);
-      digitalWrite(3,HIGH);
-      digitalWrite(4,HIGH);
+      case 'e':trasiegoMacerador();
       break;
-  
+
+      case 'f':calentar();
+      break;
+
+      case 'g':
+      break;
+
+      case 'h':
+      break;
+
+      case 'i':
+      break;
+
+      case 'j':
+      break;
+
+      case 'k':
+      break;
+
+
+
 }
   }
 }
@@ -123,19 +129,79 @@ for(int i=11;i>=2;i--){
 
 
 void desactivar(){
-   digitalWrite(2,HIGH);
- digitalWrite(3,HIGH);
- digitalWrite(4,HIGH);
- digitalWrite(5,HIGH);
- digitalWrite(6,HIGH);
- digitalWrite(7,HIGH);
- digitalWrite(8,HIGH);
- digitalWrite(9,HIGH);
- digitalWrite(10,HIGH);
- digitalWrite(11,HIGH);
+  digitalWrite(2,HIGH);
+  digitalWrite(3,HIGH);
+  digitalWrite(4,HIGH);
+  digitalWrite(5,HIGH);
+  digitalWrite(6,HIGH);
+  digitalWrite(7,HIGH);
+  digitalWrite(8,HIGH);
+  digitalWrite(9,HIGH);
+  digitalWrite(10,HIGH);
+  digitalWrite(11,HIGH);
 }
 
-void entradaAgua(){
+void entradaAguaSinFiltrar(){
+  desactivar();
+  delay(3000);
+  digitalWrite(entradaAguaDirecta, LOW) ;
+
+}
+
+void entradaAguaFiltrada(){
+  desactivar();
+  delay(3000);
+  digitalWrite(entradaAguaFiltro, LOW) ;
+
+}
+
+void entradaExterior(){
+  desactivar(); 
+  delay(3000);
+  digitalWrite(entradaExterna, LOW) ;
+  delay(3000);
+  digitalWrite(salidaGeneral, LOW) ;
+  delay(3000);
+  digitalWrite(bomba,LOW);
+
+}
+
+void trasiegoOlla(){
+  desactivar(); 
+  delay(3000);
+  digitalWrite(salidaOlla, LOW) ;
+  delay(3000);
+  digitalWrite(salidaGeneral, LOW) ;
+  delay(3000);
+  digitalWrite(bomba,LOW);
+
+}
+
+void trasiegoMacerador(){
+  desactivar(); 
+  delay(3000);
+  digitalWrite(salidaMacerador, LOW) ;
+  delay(3000);
+  digitalWrite(salidaGeneral, LOW) ;
+  delay(3000);
+  digitalWrite(bomba,LOW);
+
+}
+
+void refrigererar(){
+  desactivar(); 
+  delay(3000);
+  entradaAguaSinFiltrar();
+  delay(10000);
+  digitalWrite(salidaIntercambiador, LOW) ;
+  delay(3000);
+  digitalWrite(bomba,LOW);
   
+}
+
+void calentar(){
+  desactivar(); 
+  delay(3000);
+  digitalWrite(macerador, LOW);
   
 }
